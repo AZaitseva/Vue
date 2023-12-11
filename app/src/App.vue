b0665db72640699a86a4e980fad5c98c2ada1f5e
AZaitseva <136637754+AZaitseva@users.noreply.github.com>
2023-12-11 21:16:47 +0300

    Lesson
    Lesson

---
+++ app/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Card from './components/Card.vue'
+</script>
+
+<template>
+  <div class="card__title">заголовок в app</div>
+  <svg>
+    <use href="./assets/sprite.svg#icon-fav"></use>
+  </svg>
+  <svg>
+    <use href="./assets/sprite.svg#icon-lk"></use>
+  </svg>
+
+  <Card title="Рубашка 1" :price="3000 + 500" :is-added="true">
+    <Card title="Рубашка 2" :price="3000" />
+    <Card title="Рубашка 3" :price="3000" />
+    <Card title="Рубашка 4" :price="3000" />
+    <Card title="Рубашка 1" :price="3000" />
+    <Card title="Рубашка 1" :price="3000" />
+    <button>Добавить в корзину</button>
+  </Card>
+</template>
+
+<style>
+body {
+  background-color: black;
+  color: #fff;
+}
+</style>
---
+++ app/src/components/Card.vue
@@ -0,0 +1,48 @@
+<script setup>
+// export default {
+//   data() {
+//     return {
+//       count: 0
+//     }
+//   },
+//   methods: {
+//     icriment() {
+//       this.count++
+//     }
+//   }
+// }
+import { ref } from 'vue'
+
+let count = ref(0)
+function incriment() {
+  count.value++
+}
+
+defineProps({
+  title: String,
+  price: Number,
+  isAdded: Boolean
+})
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <div class="menu"></div>
+    </div>
+  </header>
+  <div class="card">
+    <div class="card__title">{{ title }}</div>
+    <div class="card__price">{{ price }}</div>
+    <!-- <h2>{{ count }}</h2>
+    <button @click="incriment()">Добавить 1</button>
+    <input v-model="count" /> -->
+  </div>
+</template>
+
+<style scoped>
+/* body {
+  background-color: #000;
+  color: #fff;
+} */
+</style>
