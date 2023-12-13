0af9868721236648298c0b2c2a24cf030025bc76
AZaitseva <136637754+AZaitseva@users.noreply.github.com>
2023-12-13 21:33:47 +0300

    Upload

--- app/src/App.vue
+++ app/src/App.vue
@@ -3,27 +3,115 @@ import Card from './components/Card.vue'
 </script>
 
 <template>
-  <div class="card__title">заголовок в app</div>
-  <svg>
-    <use href="./assets/sprite.svg#icon-fav"></use>
-  </svg>
-  <svg>
-    <use href="./assets/sprite.svg#icon-lk"></use>
-  </svg>
-
-  <Card title="Рубашка 1" :price="3000 + 500" :is-added="true">
-    <Card title="Рубашка 2" :price="3000" />
-    <Card title="Рубашка 3" :price="3000" />
-    <Card title="Рубашка 4" :price="3000" />
-    <Card title="Рубашка 1" :price="3000" />
-    <Card title="Рубашка 1" :price="3000" />
-    <button>Добавить в корзину</button>
-  </Card>
+  <div class="wrapper">
+    <header class="header">
+      <div class="container">
+        <div class="header__row">
+          <div class="header__logo logo">
+            <div class="logo__image">
+              <img src="./assets/logo.svg" alt="" />
+            </div>
+            <div class="logo__right">
+              <div class="logo__name">REACT SNEAKERS</div>
+              <div class="logo__desc">Магазин лучших кроссовок</div>
+            </div>
+          </div>
+          <div class="header__menu menu">
+            <nav class="menu__nav">
+              <ul class="menu__ist">
+                <li class="menu__item">
+                  <a href="" class="menu__link">
+                    <svg class="menu__icon menu__icon--cart">
+                      <use href="./assets/sprite.svg#icon-cart"></use>
+                    </svg>
+                    1205 руб.
+                  </a>
+                  <svg class="icon icon-cart"><use href="./assets/sprite.svg#icon-cart"></use></svg>
+                </li>
+                <li class="menu__item">
+                  <a href="" class="menu__link">
+                    <svg class="menu__icon"><use href="./assets/sprite.svg#icon-fav"></use></svg>
+                    Закладки</a
+                  >
+                </li>
+                <li class="menu__item">
+                  <a href="" class="menu__link">
+                    <svg class="menu__icon"><use href="./assets/sprite.svg#icon-prof"></use></svg>
+                    Профиль</a
+                  >
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+    <main class="main"></main>
+  </div>
 </template>
 
 <style>
 body {
-  background-color: black;
-  color: #fff;
+  background: #e7f6ff;
+  padding-top: 85px;
+}
+.wraper {
+  max-width: 1080px;
+  margin: 0 auto;
+  border-radius: 20px;
+  background: #fff;
+  box-shadow: 0px 10px 20px 0px rgba(0, 0, 0, 0.04);
+  min-height: 90vx;
+}
+.container {
+  max-width: 960px;
+  margin: 0 auto;
+  width: 96%;
+}
+
+.header {
+  padding: 44px 0;
+  border-bottom: 1px solid #eaeaea;
+}
+.header__row {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.logo {
+  display: flex;
+  align-items: center;
+}
+.logo__image {
+  flex-shrink: 0;
+  margin-right: 16px;
+}
+.logo__name {
+  color: #000;
+  font-family: Inter;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  text-transform: uppercase;
+}
+.logo__desc {
+  color: #9d9d9d;
+  font-family: Inter;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+
+.menu_icon {
+  height: 18px;
+  width: 18px;
+}
+.menu_icon:not(.menu_icon--cart) {
+  fill: #9b9b9b;
+}
+.menu_icon--cart {
+  stroke: #9b9b9b;
 }
 </style>
